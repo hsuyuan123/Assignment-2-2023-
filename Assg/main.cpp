91bc549b8fa96ed1deeaf20082b81f7e3f646445
hsuyuan123 <s10223040@connect.np.edu.sg>
2023-01-25 15:04:51 +0800

    Added code to read/write from file

--- Assg/main.cpp
+++ Assg/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
 #include "user.h"
 #include "topic.h"
 using namespace std;
@@ -8,6 +10,7 @@ void registeruser(){
 	string username;
 	string password;
 	cout << "Input username:";
+	//check if username is in database
 	cin >> username;
 	cout << "Input password:";
 	cin >> password;
@@ -25,7 +28,40 @@ void createtopic() {
 	Topic topic1(topicname, topictext);
 }
 void displaytopics() {
-	cout<<""
+	string input;
+	cout << "TOPICS";
+	//print all topic names, numbered from 1 to 5
+	cin >> input;
+	//if input == 6, move to next page
+	//if input == 7, createtopic()
+}
+
+void openfiles() {
+	//open file
+	ofstream fw("topic.txt", std::ofstream::out);
+	//write into file
+	if (fw.is_open())
+	{
+		//store array contents to text file
+		for (int i = 0; i < sizeof(Topic); i++) {
+			fw << Topic[i] << "\n";
+		}
+		fw.close();
+	}
+	else cout << "Problem with opening file";
+	//read from file
+	string line;
+	ifstream myfile("topic.txt");
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			cout << line << '\n';
+			vector<string> arr;
+			arr.push_back(line);
+		}
+		myfile.close();
+	}
 }
 
 int main(){
